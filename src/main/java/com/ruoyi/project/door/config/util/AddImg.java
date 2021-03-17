486541607512c50999e3a7a5f5d648402feef020
sweet <sweet98912@163.com>
2021-03-17 09:45:25 +0800

    C：修改上传文件的文件名，不要随机码，只要文件名

--- src/main/java/com/ruoyi/project/door/config/util/AddImg.java
+++ src/main/java/com/ruoyi/project/door/config/util/AddImg.java
@@ -25,13 +25,14 @@ public class AddImg {
             String originalFilename = file.getOriginalFilename();
             System.out.println("originalFilename"+originalFilename);
             //新的文件名字  在你图片原始名字的前面添加随机数 不会出现名字重复的问题
-            String newFileName = UUID.randomUUID() + originalFilename;
+//            String newFileName = UUID.randomUUID() + originalFilename;
             //封装上传文件位置的全路径  把你要保存的文件的路径和图片的新名字连到一起，得到这个文件位置的全路径
-            File targetFile = new File(filePath, newFileName);
+            File targetFile = new File(filePath,originalFilename);
+//            File targetFile = new File(filePath, newFileName);
             System.out.println("targetFile"+targetFile);//targetFileE:\eCloud\blog-test\test\src\main\webapp\images\71bbd566-1c64-45e0-8b6c-4929582ef47bQQ图片20200726223852.png
             //把本地文件  上传到 封装上传文件位置的全路径 主要把MultipartFile转换为File
             file.transferTo(targetFile);
-            user.setOriginalPicture(filePath+"\\"+newFileName);
+            user.setOriginalPicture(filePath+"\\"+originalFilename);
 
         } catch (Exception e) {
 
@@ -67,13 +68,13 @@ public class AddImg {
             String newOriginalFilename = newFile.getOriginalFilename();
             System.out.println("originalFilename"+newOriginalFilename);
             //新的文件名字  在你图片原始名字的前面添加随机数 不会出现名字重复的问题
-            String newNewFileName = UUID.randomUUID() + newOriginalFilename;
+//            String newNewFileName = UUID.randomUUID() + newOriginalFilename;
             //封装上传文件位置的全路径  把你要保存的文件的路径和图片的新名字连到一起，得到这个文件位置的全路径
-            File newTargetFile = new File(newFilePath, newNewFileName);
+            File newTargetFile = new File(newFilePath, newOriginalFilename);
             System.out.println("newTargetFile"+newTargetFile);//targetFileE:\eCloud\blog-test\test\src\main\webapp\images\71bbd566-1c64-45e0-8b6c-4929582ef47bQQ图片20200726223852.png
             //把本地文件  上传到 封装上传文件位置的全路径 主要把MultipartFile转换为File
             newFile.transferTo(newTargetFile);
-            doorOpenRecord.setContrastingPicture(newFilePath+"\\"+newNewFileName);
+            doorOpenRecord.setContrastingPicture(newFilePath+"\\"+newOriginalFilename);
         } catch (Exception e) {
 
             e.printStackTrace();
